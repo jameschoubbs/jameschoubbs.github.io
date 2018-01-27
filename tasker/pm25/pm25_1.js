8e914a2b59bf0acb57ebe6274babf40a4a8079fa
jameschoubbs <jameschoubbs@gmail.com>
2018-01-27 10:52:05 +0800

    Add WSHOM2 to test.

--- tasker/pm25/pm25_1.js
+++ tasker/pm25/pm25_1.js
@@ -1,17 +1,11 @@
+var sheetUrl_WSHOM1 = 'https://docs.google.com/spreadsheets/d/1FOtBgVQuru_b91fbc1VyErKqqGL67EuhulgQPqjfCqU/edit?usp=sharing';
+var sheetName_WSHOM1 = 'WSHOM1Data';
+var deviceId_WSHOM1 = '10VWD5KQ';
+var sheetUrl_WSHOM2 = 'https://docs.google.com/spreadsheets/d/1-Nvc0tMEwlzhPeva8XXTNky9TXB5teVTOi6i_Avjb3I/edit?usp=sharing';
+var sheetName_WSHOM2 = 'Data';
+var deviceId_WSHOM2 = '10VggPwV';
+
 window.addEventListener('load', function(e) {
-	var ConnSta;
-	var g3;
-	var myData;
-	var YYYY;
-	var MM;
-	var DD;
-	var DateStr;
-	var HH;
-	var MI;
-	var SS;
-	var TimeStr;
-	var PM2_5;
-	var PM1_0;
 
 	function get_date(t) {
 		var varDay = new Date(),
@@ -53,54 +47,97 @@ window.addEventListener('load', function(e) {
 		return varNow;
 	}
 
+	var fBoardToRun = function(elementId, deviceId, sheetUrl, sheetName)	{
+		
+		var ConnSta = '嘗試連線...';
+		document.getElementById(elementId).innerHTML = ConnSta;
 
-	ConnSta = '嘗試連線...';
-	document.getElementById("demo-area-01-show").innerHTML = ConnSta;
-
-	boardReady({board: 'Smart', device: '10VWD5KQ', transport: 'mqtt'}, function (board) {
-		board.samplingInterval = 50;
-		board.on('error',function (err) {
-			board.error = err;
-			document.getElementById("demo-area-01-show").innerHTML = '連線中斷！';
+		boardReady({ board: 'Smart', device: deviceId, transport: 'mqtt' }, function (board) {
+			
+			board.samplingInterval = 50;
+			
+			board.on('error',function (err) {
+				
+				board.error = err;
+				document.getElementById(elementId).innerHTML = '連線中斷！';
+				
+				setTimeout(function() { fBoardToRun(elementId, deviceId, sheetUrl, sheetName); }, 1000);
+				
+			});
+			
+			var g3 = getG3(board, 14, 3);
+			
+			var myData= {};
+			
+			myData.sheetUrl = sheetUrl;
+			myData.sheetName = sheetName;
+			
+			g3.read(function(evt) {
+				
+				var YYYY = String('') + String(get_date("y"));
+				var MM = String('') + String(get_date("m"));
+				
+				if (get_date("m") < 10) {
+					
+					MM = String('0') + String(get_date("m"));
+					
+				}
+				
+				var DD = String('') + String(get_date("d"));
+				
+				if (get_date("d") < 10) {
+					
+					DD = String('0') + String(get_date("d"));
+					
+				}
+				
+				var DateStr = [YYYY,'-',MM,'-',DD].join('');
+				var HH = String('') + String(get_time("h"));
+				
+				if (get_time("h") < 10) {
+					
+					HH = String('0') + String(get_time("h"));
+					
+				}
+				
+				var MI = String('') + String(get_time("m"));
+				
+				if (get_time("m") < 10) {
+					
+					MI = String('0') + String(get_time("m"));
+					
+				}
+				
+				var SS = String('') + String(get_time("s"));
+				
+				if (get_time("s") < 10) {
+					
+					SS = String('0') + String(get_time("s"));
+					
+				}
+				
+				var TimeStr = [HH,':',MI,':',SS].join('');
+				
+				ConnSta = '正在監測...';
+				
+				var PM2_5 = g3.pm25;
+				var PM1_0 = g3.pm10;
+				
+				if (PM2_5 != 0) {
+					
+					myData.column0 = [DateStr,' ',TimeStr].join('');
+					myData.column1 = PM2_5;
+					myData.column2 = PM1_0;
+					
+					writeSheetData(myData);
+					
+					document.getElementById(elementId).innerHTML = ([ConnSta,('<br/>'),DateStr,('<br/>'),TimeStr,('<br/>'),'PM2.5=[',PM2_5,']',('<br/>'),'PM1.0=[',PM1_0,']'].join(''));
+				}
+				
+			}, 1000);
 		});
-		g3 = getG3(board, 14,3);
-		myData= {};
-		myData.sheetUrl = 'https://docs.google.com/spreadsheets/d/1FOtBgVQuru_b91fbc1VyErKqqGL67EuhulgQPqjfCqU/edit?usp=sharing';
-		myData.sheetName = 'WSHOM1Data';
-		g3.read(function(evt){
-			YYYY = String('') + String(get_date("y"));
-			MM = String('') + String(get_date("m"));
-			if (get_date("m") < 10) {
-				MM = String('0') + String(get_date("m"));
-			}
-			DD = String('') + String(get_date("d"));
-			if (get_date("d") < 10) {
-				DD = String('0') + String(get_date("d"));
-			}
-			DateStr = [YYYY,'-',MM,'-',DD].join('');
-			HH = String('') + String(get_time("h"));
-			if (get_time("h") < 10) {
-				HH = String('0') + String(get_time("h"));
-			}
-			MI = String('') + String(get_time("m"));
-			if (get_time("m") < 10) {
-				MI = String('0') + String(get_time("m"));
-			}
-			SS = String('') + String(get_time("s"));
-			if (get_time("s") < 10) {
-				SS = String('0') + String(get_time("s"));
-			}
-			TimeStr = [HH,':',MI,':',SS].join('');
-			ConnSta = '正在監測...';
-			PM2_5 = g3.pm25;
-			PM1_0 = g3.pm10;
-			if (PM2_5 != 0) {
-				myData.column0 = [DateStr,' ',TimeStr].join('');
-				myData.column1 = PM2_5;
-				myData.column2 = PM1_0;
-				writeSheetData(myData);
-				document.getElementById("demo-area-01-show").innerHTML = ([ConnSta,("<br/>"),DateStr,("<br/>"),TimeStr,("<br/>"),'PM2.5=[',PM2_5,']',("<br/>"),'PM1.0=[',PM1_0,']'].join(''));
-			}
-		}, 1000);
-	});
+	};
+	
+	fBoardToRun('demo-area-02-show', deviceId_WSHOM2, sheetUrl_WSHOM2, sheetName_WSHOM2);
+
 });
