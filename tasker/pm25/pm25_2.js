2284886ad820eff023d2a7879d2063f97d59f845
jameschoubbs <jameschoubbs@gmail.com>
2018-02-07 17:45:05 +0800

    Testing display real spreadsheet.

--- tasker/pm25/pm25_2.js
+++ tasker/pm25/pm25_2.js
@@ -1,85 +1,77 @@
-// Client ID and API key from the Developer Console
 var CLIENT_ID = '656523659985-o5feg4jk9og2jrd76cdt2pvl0e0lagor.apps.googleusercontent.com';
 var API_KEY = 'AIzaSyAnmqStyPEFVtdqK8h7MlBF1xEPtGLh6As';
 
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+var DISCOVERY_DOCS = ['https://sheets.googleapis.com/$discovery/rest?version=v4'];
+var SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+var WSHOM1_DocId = '1FOtBgVQuru_b91fbc1VyErKqqGL67EuhulgQPqjfCqU';
+var WSHOM1_SheetRange = 'TestValue!A1:M2';
 
-/**
- *  On load, called to load the auth2 library and API client library.
- */
 function handleClientLoad() {
+
 	gapi.load('client:auth2', initClient);
+
 }
 
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
 function initClient() {
+
 	gapi.client.init({
+
 		apiKey: API_KEY,
 		clientId: CLIENT_ID,
 		discoveryDocs: DISCOVERY_DOCS,
 		scope: SCOPES
+
 	}).then(function () {
-		// Listen for sign-in state changes.
+
 		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-		// Handle the initial sign-in state.
 		updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+
 	});
+
 }
 
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
 function updateSigninStatus(isSignedIn) {
+
 	if (isSignedIn) {
-		listMajors();
+
+		Refresh();
+
 	} else {
+
 		gapi.auth2.getAuthInstance().signIn();
+
 	}
+
 }
 
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message) {
-	var pre = document.getElementById('content');
-	var textContent = document.createTextNode(message + '\n');
-	pre.appendChild(textContent);
-}
+function Refresh() {
 
-/**
- * Print the names and majors of students in a sample spreadsheet:
- * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- */
-function listMajors() {
 	gapi.client.sheets.spreadsheets.values.get({
-		spreadsheetId: '1yg0KgxWTWm9V0AdKcc_czzTr9H50AZYExem3BQvpALI',
-		range: 'Data!A1:C10',
+
+		spreadsheetId: WSHOM1_DocId,
+		range: WSHOM1_SheetRange,
+
 	}).then(function(response) {
+
+		var element = document.getElementById('demo-area-01-show');
+
 		var range = response.result;
-		if (range.values.length > 0) {
-			appendPre('Time, PM2.5, PM1.0:');
-			for (i = 0; i < range.values.length; i++) {
-				var row = range.values[i];
-				// Print columns A and E, which correspond to indices 0 and 4.
-				appendPre(row[0] + ', ' + row[1] + ', ' + row[2]);
-			}
-		} else {
-			appendPre('No data found.');
+
+		if (range.values.length > 1) {
+
+			var row = range.values[1];
+			
+			element.innerHTML = row[0];
+
 		}
+		else {
+
+		}
+
 	}, function(response) {
-		appendPre('Error: ' + response.result.error.message);
+
 	});
+
 }
