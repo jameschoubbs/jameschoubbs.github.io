4070b39bbbba89bf6d38b0d3ccccbbdff667399e
jameschoubbs <jameschoubbs@gmail.com>
2018-02-08 15:54:07 +0800

    Add exception handler.

--- tasker/pm25/pm25_2.js
+++ tasker/pm25/pm25_2.js
@@ -67,188 +67,195 @@ function RefreshViewerElement(elementId, deviceComment, docId, sheetRange) {
 
 	var fUpdate = function () {
 
-		gapi.client.sheets.spreadsheets.values.get({
+		try {
 
-			spreadsheetId: docId,
-			range: sheetRange,
+			gapi.client.sheets.spreadsheets.values.get({
 
-		}).then(function (response) {
+				spreadsheetId: docId,
+				range: sheetRange,
 
-			isReady = true;
+			}).then(function (response) {
 
-			var range = response.result;
+				isReady = true;
 
-			if (range.values.length > 1) {
+				var range = response.result;
 
-				connSta = deviceComment + ' 正在監測中...';
+				if (range.values.length > 1) {
 
-				var row = range.values[1];
+					connSta = deviceComment + ' 正在監測中...';
 
-				if (row != null &&
-					row.length > 0) {
+					var row = range.values[1];
 
-					var isOK = true;
-					var dateTimeParts = null;
-					var dateStr = null;
-					var timeStr = null;
-					var pm25Value = null;
-					var pm10Value = null;
-					var pm25Value60Max = null;
+					if (row != null &&
+						row.length > 0) {
 
-					// (4)[量測時間]
-					isOK = (isOK && (row[3] != null && row[3].length > 0));
+						var isOK = true;
+						var dateTimeParts = null;
+						var dateStr = null;
+						var timeStr = null;
+						var pm25Value = null;
+						var pm10Value = null;
+						var pm25Value60Max = null;
 
-					if (isOK) {
+						// (4)[量測時間]
+						isOK = (isOK && (row[3] != null && row[3].length > 0));
 
-						dateTimeParts = row[3].split(' ');
+						if (isOK) {
 
-						if (dateTimeParts != null &&
-							dateTimeParts.length > 1) {
+							dateTimeParts = row[3].split(' ');
 
-							dateStr = dateTimeParts[0];
-							timeStr = dateTimeParts[1];
+							if (dateTimeParts != null &&
+								dateTimeParts.length > 1) {
+
+								dateStr = dateTimeParts[0];
+								timeStr = dateTimeParts[1];
+							}
+							else {
+
+								isOK = false;
+							}
+						}
+
+						// (12)[最新PM2.5偵測值]
+						isOK = (isOK && (row[11] != null && row[11].length > 0 && !isNaN(row[11])));
+
+						if (isOK) {
+
+							pm25Value = row[11];
+						}
+
+						// (13)[最新PM1.0偵測值]
+						isOK = (isOK && (row[12] != null && row[12].length > 0 && !isNaN(row[12])));
+
+						if (isOK) {
+
+							pm10Value = row[12];
+						}
+
+						// (1)[近60筆PM2.5最大值]
+						isOK = (isOK && (row[0] != null && row[0].length > 0 && !isNaN(row[0])));
+
+						if (isOK) {
+
+							pm25Value60Max = row[0];
+						}
+
+						if (isOK) {
+
+							var pm25Text = '(無)';
+
+							if (pm25Value >= 0 && pm25Value <= 11) {
+
+								pm25Text = '(低)';
+								element.style.color = '#000';
+								element.style.backgroundColor = '#9cff9c';
+								element.style.borderColor = 'gray';
+							}
+							else if (pm25Value >= 12 && pm25Value <= 23) {
+
+								pm25Text = '(低)';
+								element.style.color = '#000';
+								element.style.backgroundColor = '#31ff00';
+								element.style.borderColor = 'gray';
+							}
+							else if (pm25Value >= 24 && pm25Value <= 35) {
+
+								pm25Text = '(低)';
+								element.style.color = '#000';
+								element.style.backgroundColor = '#31cf00';
+								element.style.borderColor = 'gray';
+							}
+							else if (pm25Value >= 36 && pm25Value <= 41) {
+
+								pm25Text = '(中)';
+								element.style.color = '#000';
+								element.style.backgroundColor = 'yellow';
+								element.style.borderColor = 'gray';
+							}
+							else if (pm25Value >= 42 && pm25Value <= 47) {
+
+								pm25Text = '(中)';
+								element.style.color = '#000';
+								element.style.backgroundColor = 'ffcf00';
+								element.style.borderColor = 'gray';
+							}
+							else if (pm25Value >= 48 && pm25Value <= 53) {
+
+								pm25Text = '(中)';
+								element.style.color = '#000';
+								element.style.backgroundColor = '#ff9a00';
+								element.style.borderColor = 'gray';
+							}
+							else if (pm25Value >= 54 && pm25Value <= 58) {
+
+								pm25Text = '(高)';
+								element.style.color = '#000';
+								element.style.backgroundColor = '#ff6464';
+								element.style.borderColor = 'gray';
+							}
+							else if (pm25Value >= 59 && pm25Value <= 64) {
+
+								pm25Text = '(高)';
+								element.style.color = 'gray';
+								element.style.backgroundColor = '#red';
+								element.style.borderColor = 'gray';
+							}
+							else if (pm25Value >= 65 && pm25Value <= 70) {
+
+								pm25Text = '(高)';
+								element.style.color = 'gray';
+								element.style.backgroundColor = '#900';
+								element.style.borderColor = 'gray';
+							}
+							else if (pm25Value >= 71) {
+
+								pm25Text = '(非常高)';
+								element.style.color = 'white';
+								element.style.backgroundColor = '#ce30ff';
+								element.style.borderColor = 'gray';
+							}
+
+							element.innerHTML =
+								([
+									connSta, ('<br/>'),
+									dateStr, ('&nbsp;'), timeStr, ('<br/>'),
+									'PM2.5=[', pm25Value, ']', ('&nbsp;'), pm25Text, ('&nbsp;'), '<span name="pm25max">Max=[', pm25Value60Max, ']</span>', ('<br/>'),
+									'PM1.0=[', pm10Value, ']'
+								].join(''));
+
+							lastDateTime = dateStr + ' ' + timeStr;
 						}
 						else {
 
-							isOK = false;
 						}
 					}
 
-					// (12)[最新PM2.5偵測值]
-					isOK = (isOK && (row[11] != null && row[11].length > 0 && !isNaN(row[11])));
+					setTimeout(function () {
 
-					if (isOK) {
+						fUpdate();
 
-						pm25Value = row[11];
-					}
+					}, 1000);
+				}
+				else {
 
-					// (13)[最新PM1.0偵測值]
-					isOK = (isOK && (row[12] != null && row[12].length > 0 && !isNaN(row[12])));
+					element.style.color = 'gray';
+					element.style.backgroundColor = 'black';
+					element.style.borderColor = 'green';
 
-					if (isOK) {
+					connSta = deviceComment + ' 連線中斷！' + initRow;
+					element.innerHTML = connSta;
 
-						pm10Value = row[12];
-					}
-
-					// (1)[近60筆PM2.5最大值]
-					isOK = (isOK && (row[0] != null && row[0].length > 0 && !isNaN(row[0])));
-
-					if (isOK) {
-
-						pm25Value60Max = row[0];
-					}
-
-					if (isOK) {
-
-						var pm25Text = '(無)';
-
-						if (pm25Value >= 0 && pm25Value <= 11) {
-
-							pm25Text = '(低)';
-							element.style.color = '#000';
-							element.style.backgroundColor = '#9cff9c';
-							element.style.borderColor = 'gray';
-						}
-						else if (pm25Value >= 12 && pm25Value <= 23) {
-
-							pm25Text = '(低)';
-							element.style.color = '#000';
-							element.style.backgroundColor = '#31ff00';
-							element.style.borderColor = 'gray';
-						}
-						else if (pm25Value >= 24 && pm25Value <= 35) {
-
-							pm25Text = '(低)';
-							element.style.color = '#000';
-							element.style.backgroundColor = '#31cf00';
-							element.style.borderColor = 'gray';
-						}
-						else if (pm25Value >= 36 && pm25Value <= 41) {
-
-							pm25Text = '(中)';
-							element.style.color = '#000';
-							element.style.backgroundColor = 'yellow';
-							element.style.borderColor = 'gray';
-						}
-						else if (pm25Value >= 42 && pm25Value <= 47) {
-
-							pm25Text = '(中)';
-							element.style.color = '#000';
-							element.style.backgroundColor = 'ffcf00';
-							element.style.borderColor = 'gray';
-						}
-						else if (pm25Value >= 48 && pm25Value <= 53) {
-
-							pm25Text = '(中)';
-							element.style.color = '#000';
-							element.style.backgroundColor = '#ff9a00';
-							element.style.borderColor = 'gray';
-						}
-						else if (pm25Value >= 54 && pm25Value <= 58) {
-
-							pm25Text = '(高)';
-							element.style.color = '#000';
-							element.style.backgroundColor = '#ff6464';
-							element.style.borderColor = 'gray';
-						}
-						else if (pm25Value >= 59 && pm25Value <= 64) {
-
-							pm25Text = '(高)';
-							element.style.color = 'gray';
-							element.style.backgroundColor = '#red';
-							element.style.borderColor = 'gray';
-						}
-						else if (pm25Value >= 65 && pm25Value <= 70) {
-
-							pm25Text = '(高)';
-							element.style.color = 'gray';
-							element.style.backgroundColor = '#900';
-							element.style.borderColor = 'gray';
-						}
-						else if (pm25Value >= 71) {
-
-							pm25Text = '(非常高)';
-							element.style.color = 'white';
-							element.style.backgroundColor = '#ce30ff';
-							element.style.borderColor = 'gray';
-						}
-
-						element.innerHTML =
-							([
-								connSta, ('<br/>'),
-								dateStr, ('&nbsp;'), timeStr, ('<br/>'),
-								'PM2.5=[', pm25Value, ']', ('&nbsp;'), pm25Text, ('&nbsp;'), '<span name="pm25max">Max=[', pm25Value60Max, ']</span>', ('<br/>'),
-								'PM1.0=[', pm10Value, ']'
-							].join(''));
-
-						lastDateTime = dateStr + ' ' + timeStr;
-					}
-					else {
-
-					}
+					setTimeout(function () { fUpdate(); }, 3000);
 				}
 
-				setTimeout(function () {
+			}, function (response) {
 
-					fUpdate();
+			});
+		}
+		catch (ex) {
 
-				}, 1000);
-			}
-			else {
-
-				element.style.color = 'gray';
-				element.style.backgroundColor = 'black';
-				element.style.borderColor = 'green';
-
-				connSta = deviceComment + ' 連線中斷！' + initRow;
-				element.innerHTML = connSta;
-
-				setTimeout(function () { fUpdate(); }, 3000);
-			}
-
-		}, function (response) {
-
-		});
+			setTimeout(function () { fUpdate(); }, 3000);
+		}
 	};
 
 	fUpdate();
